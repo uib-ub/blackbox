f8ee6966687945b4d7291fc921b3a5c15c9703fc
hemed <hemed_ali@hotmail.com>
2016-01-06 16:17:06 +0100

    Trying post_filter

--- src/main/java/no/uib/marcus/search/MarcusSearchService.java
+++ src/main/java/no/uib/marcus/search/MarcusSearchService.java
@@ -1,17 +1,22 @@
 package no.uib.marcus.search;
 
 import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import no.uib.marcus.search.client.ClientFactory;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.query.BoolFilterBuilder;
 import org.elasticsearch.index.query.FilterBuilder;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryFilterBuilder;
 import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.filters.FiltersAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilderException;
@@ -67,13 +72,19 @@ public class MarcusSearchService implements SearchService {
 
     /**
      * Get All Documents using query string. 
-          *
      */
     @Override
     public SearchResponse getAllDocuments(String queryStr, String indexName, String typeName, Map<String, String> aggMap) {
         SearchResponse response = null;
         //See this: http://stackoverflow.com/questions/23807314/multiple-filters-and-an-aggregate-in-elasticsearch
-         BoolFilterBuilder fb = (BoolFilterBuilder)FilterBuilders.boolFilter().must(FilterBuilders.termFilter("status", "Sent"));
+              BoolFilterBuilder fb = (BoolFilterBuilder)FilterBuilders
+                .boolFilter()
+                //.must(FilterBuilders.boolFilter()
+               //.should(FilterBuilders.termFilter("status", "Draft"))
+               //  .should(FilterBuilders.termsFilter("assigned_to", "Marianne Paasche", "Morten Heiselberg"))
+                 .must(FilterBuilders.termsFilter("status", "Estimate" , "Draft"))
+               //.must(FilterBuilders.termFilter("assigned_to", "Marianne Paasche"))
+                ;
          
          //FiltersAggregationBuilders agb =  (FiltersAggregationBuilders)AggregationBuilders.filters("filtered").filter("filter", fb );
          
@@ -81,10 +92,12 @@ public class MarcusSearchService implements SearchService {
                 response = ClientFactory.getTransportClient()
                     .prepareSearch(indexName)
                     .setTypes(typeName)
+                    //.setQuery(QueryBuilders.filteredQuery(QueryBuilders.queryStringQuery(queryStr), fb))
+                    .setPostFilter(fb)
                     .setQuery(QueryBuilders.queryStringQuery(queryStr))
                     .addAggregation(AggregationBuilders.terms("status").field("status"))
                     .addAggregation(AggregationBuilders.terms("assigned_to").field("assigned_to"))
-                    .addAggregation(AggregationBuilders.filters("filtered").filter("filter", fb))
+                    //.addAggregation(AggregationBuilders.filter("filtered").filter(fb))
                     .execute()
                     .actionGet();
         } catch (SearchSourceBuilderException se) {
@@ -94,26 +107,42 @@ public class MarcusSearchService implements SearchService {
         return response;
     }
     
-    public static void testAggRes(){
+    
+    //Testing Aggregations
+    public static void testAggRes() throws IOException{
         //http://stackoverflow.com/questions/23807314/multiple-filters-and-an-aggregate-in-elasticsearch
         BoolFilterBuilder fb = (BoolFilterBuilder)FilterBuilders
                 .boolFilter()
                 .must(FilterBuilders.termFilter("status", "Sent"))
                 .must(FilterBuilders.termFilter("assigned_to", "Marianne Paasche"));
         
+        AggregationBuilder aggregation = AggregationBuilders
+                                    .filters("agg")
+                                    .filter("men", FilterBuilders.termFilter("gender", "male"))
+                                    .filter("women", FilterBuilders.termFilter("gender", "female"));
+                
+                XContentBuilder jsonObj = XContentFactory.jsonBuilder()
+                            .startObject()
+                            .field("filtered", XContentFactory.jsonBuilder().field("filter",fb))
+                            .endObject();
+         
                  SearchRequestBuilder req = ClientFactory.getTransportClient()
                 .prepareSearch("admin")
                 .setTypes("eddie")
-                .setQuery(QueryBuilders.queryStringQuery("mari"))
-                .addAggregation(AggregationBuilders.filter("filtered").filter(fb))
-                .addAggregation(AggregationBuilders.terms("status").field("status"))
-                .addAggregation(AggregationBuilders.terms("assigned_to").field("assigned_to"));
+                .setQuery(QueryBuilders.filteredQuery(QueryBuilders.queryStringQuery("mari"), fb))
+                //.setPostFilter()
+                ;
+                //.addAggregation(aggregation)
+                //.addAggregation(AggregationBuilders.filter("filtered").filter(fb))
+                //.addAggregation(AggregationBuilders.terms("status").field("status"))
+                //.addAggregation(AggregationBuilders.terms("assigned_to").field("assigned_to"));
                      
-        System.out.println(req.toString());
+        System.out.println(req);
+         System.out.println(aggregation);
     }
 
     //Main method for easy debugging
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Gson gson = new Gson();
         //System.out.println(getAll("admin", null));
         //System.out.println(getAllDocuments("ma", "admin" , "invoice", null));
