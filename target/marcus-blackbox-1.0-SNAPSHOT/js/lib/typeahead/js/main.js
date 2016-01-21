d9c87f2b4d4891a0741b105487597bcc1864ae16
hemed <hemed_ali@hotmail.com>
2016-01-21 17:32:50 +0100

    Configure log4j

---
+++ target/marcus-blackbox-1.0-SNAPSHOT/js/lib/typeahead/js/main.js
@@ -0,0 +1,141 @@
+$(document).ready(function () {
+    var engine, remoteHost, template, empty;
+
+    $.support.cors = true;
+
+    remoteHost = 'https://typeahead-js-twitter-api-proxy.herokuapp.com';
+    template = Handlebars.compile($("#result-template").html());
+    empty = Handlebars.compile($("#empty-template").html());
+
+    engine = new Bloodhound({
+        identify: function (o) {
+            return o.id_str;
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name', 'screen_name'),
+        dupDetector: function (a, b) {
+            return a.id_str === b.id_str;
+        },
+        prefetch: remoteHost + '/demo/prefetch',
+        remote: {
+            url: remoteHost + '/demo/search?q=%QUERY',
+            wildcard: '%QUERY'
+        }
+    });
+
+    // ensure default users are read on initialization
+    engine.get('1090217586', '58502284', '10273252', '24477185')
+
+    function engineWithDefaults(q, sync, async) {
+        if (q === '') {
+            sync(engine.get('1090217586', '58502284', '10273252', '24477185'));
+            async([]);
+        }
+        else {
+            engine.search(q, sync, async);
+        }
+    }
+    
+    /**
+     *===============================================================
+     *Typeahead and Bloodhound settings for Marcus suggestion engine
+     *=============================================================== 
+     */
+    
+    //Initialize suggestion engine
+    var marcusEngine = new Bloodhound({
+        name: 'suggest',
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: 'suggest?q=m',
+        limit: 10,
+        remote: {
+            url: 'suggest?q=%QUERY',
+            wildcard:'%QUERY',
+        rateLimitWait: 20
+        }
+     });
+     
+    //Set default suggestion
+    function marcusEngineWithDefaults(q, sync, async) {
+     if (q === '') {
+       sync(['Hemed Ali', 'Tarje Lævik', 'Øyvind Gjesdal' , 'Biledsamlingen','Manuskripter','Dokumenter','Personer']);
+       async([]);
+     }
+     else {
+        marcusEngine.search(q, sync, async);
+       }
+     }
+    
+    //Put settings to Typeahead
+    $('#search-input').typeahead({
+        hint: $('.Typeahead-hint'),
+        menu: $('.Typeahead-menu'),
+        minLength: 0,
+        highlight: true,
+        classNames: {
+            open: 'is-open',
+            empty: 'is-empty',
+            cursor: 'is-active',
+            suggestion: 'Typeahead-suggestion ProfileCard',
+            selectable: 'Typeahead-selectable'
+        }
+     }, 
+        {
+            name: 'suggest',
+            source: marcusEngineWithDefaults,
+            limit: 5
+            /**templates: {
+             suggestion: '<div>{{value}}</div>'
+            }**/
+        })
+            .on('typeahead:asyncrequest', function () {
+                $('.Typeahead-spinner').show();
+        })
+            .on('typeahead:asynccancel typeahead:asyncreceive', function () {
+                $('.Typeahead-spinner').hide();
+            })
+              //When suggestion value is selected, update angular contoller with new value
+              .on('typeahead:select', function(event, suggestionValue){
+                 var angularScope = angular.element($('#searchController')).scope();
+                     angularScope.query_string = suggestionValue;
+                     angularScope.query_search();
+                angularScope.$apply();
+            })
+            //When Enter key is clicked, close the suggestion box.
+            .on('keyup', function (event) {
+                if (event.which === 13) {
+                  $('#search-input').typeahead('close');
+                }
+            });
+
+
+            /**$('#demo-input').typeahead({
+             hint: $('.Typeahead-hint'),
+             menu: $('.Typeahead-menu'),
+             minLength: 0,
+             highlight: true,
+             classNames: {
+             open: 'is-open',
+             empty: 'is-empty',
+             cursor: 'is-active',
+             suggestion: 'Typeahead-suggestion',
+             selectable: 'Typeahead-selectable'
+             }
+             }, {
+             source: engineWithDefaults
+             displayKey: 'screen_name',
+             templates: {
+             suggestion: template,
+             empty: empty
+             }
+             })
+             .on('typeahead:asyncrequest', function () {
+             $('.Typeahead-spinner').show();
+             })
+             .on('typeahead:asynccancel typeahead:asyncreceive', function () {
+             $('.Typeahead-spinner').hide();
+             });**/
+
+
+});
---
+++ target/marcus-blackbox-1.0-SNAPSHOT/js/marcus-controller.js
@@ -0,0 +1,98 @@
+'use strict';
+//Contoller file for Marcus-search system
+
+var app = angular.module('marcus', ["checklist-model", "settings"]);
+//Predefined aggregations
+//.constant('aggs', '[{"field": "status", "size": 15} , {"field" : "assigned_to"}]');
+
+/**app.controller('showAllResults',
+ function ($scope, $http) {
+ $http({method: 'GET', url: 'discover'})
+ 
+ .success(function (data, status, headers, config) {
+ $scope.results = data;
+ //alert(JSON.stringify(data));
+ })
+ 
+ .error(function (data, status, headers, config) {
+ $scope.log = 'Error occured while querying';
+ });
+ });**/
+
+/**app.controller('checkBoxCtrl', function ($scope) {
+ $scope.selected_facets = [];
+ if($scope.selected_facets.length > 0){
+ alert($scope.selected_facets[0]);
+ }
+ 
+ $scope.uncheckAll = function () {
+ $scope.selected_facets = [];
+ };
+ });**/
+
+
+app.controller('freeTextSearch', function ($scope, $http, $location, mySetting) {
+    //See here: <http://vitalets.github.io/checklist-model/>
+
+
+    $scope.selected_filters = [];
+    $scope.getCheckedValue = function (field, filterValue) {
+        if (field !== undefined && filterValue !== undefined) {
+            return field + "." + filterValue;
+        }
+        return null;
+    };
+    //alert("Predefined settings:" + JSON.stringify(mySettings));
+
+
+    //Testing for location
+    // var s = $location.search();
+    // $scope.selected_aggs.push(s.agg);
+
+    //Send requests to search servlet
+    $scope.query_search = function () {
+        var q = "";
+        $scope.query_string === undefined ? q = "*" : q = $scope.query_string + "*";
+        $http({
+            method: 'POST',
+            url: 'search?aggs=' + JSON.stringify(mySetting.facets),
+            params: {
+                q: q,
+                filter: $scope.selected_filters,
+                index: mySetting.index,
+                type: mySetting.type
+            }
+        })
+                .success(function (data, status, headers, config) {
+                    $scope.results = data;
+
+                    // If we have aggregations, store them in the scope.
+                    if (data.aggregations) {
+                        $scope.aggregations = data.aggregations;
+                    }
+                })
+                .error(function (data, status, headers, config) {
+                    $scope.log = 'Error occured while querying' + data;
+                });
+    };
+    //Suggest endpoint
+    $scope.auto_complete = function () {
+        var q = "";
+        $scope.query_string === undefined ? q = "" : q = $scope.query_string;
+        $http({
+            method: 'POST',
+            url: 'suggest',
+            params: {
+                q: q
+            }
+        })
+                .success(function (data, status, headers, config) {
+                    $scope.suggestion_list = data;
+                })
+                .error(function (data, status, headers, config) {
+                    $scope.log = 'Error occured while querying';
+                });
+    };
+    //Call this function on pageload
+    $scope.query_search();
+});
