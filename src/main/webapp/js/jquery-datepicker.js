f2ecc731fb187023904543d2fe552385c7ee9956
Tarje Sælen Lavik <Tarje.Lavik@ub.uib.no>
2016-02-24 11:13:02 +0100

    Testing filter on concept facet. Slow when done client side.

--- src/main/webapp/js/jquery-datepicker.js
+++ src/main/webapp/js/jquery-datepicker.js
@@ -5,7 +5,7 @@ $(function () {
     $("#from_date").datepicker({
         showOn: "both",
         //buttonImage: "img/calendar.gif",
-        buttonText: '<span class="glyphicon glyphicon-calendar" style="border:0"></span>',
+        buttonText: '<i class="ui icon calendar"></i>',
         changeMonth: true,
         changeYear: true,
         yearRange: "1700:+1",
@@ -20,7 +20,7 @@ $(function () {
     $("#to_date").datepicker({
         showOn: "both",
         //buttonImage: "img/calendar.gif",
-        buttonText: '<span class="glyphicon glyphicon-calendar" style="border:0"></span>',
+        buttonText: '<i class="ui icon calendar"></i>',
         changeMonth: true,
         changeYear: true,
         yearRange: "1700:+1",
