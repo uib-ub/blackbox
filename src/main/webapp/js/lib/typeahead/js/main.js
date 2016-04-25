5e64808b0eb311ec1a6b7adc728ff7a0f6fb3617
hemed <hemed_ali@hotmail.com>
2016-04-25 15:54:47 +0200

    Clean static pages

--- src/main/webapp/js/lib/typeahead/js/main.js
+++ src/main/webapp/js/lib/typeahead/js/main.js
@@ -6,41 +6,6 @@
 **/
 
 $(document).ready(function () {
-    var engine, remoteHost, template, empty;
-    $.support.cors = true;
-    
-    remoteHost = 'https://typeahead-js-twitter-api-proxy.herokuapp.com';
-    template = Handlebars.compile($("#result-template").html());
-    empty = Handlebars.compile($("#empty-template").html());
-
-    engine = new Bloodhound({
-        identify: function (o) {
-            return o.id_str;
-        },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name', 'screen_name'),
-        dupDetector: function (a, b) {
-            return a.id_str === b.id_str;
-        },
-        prefetch: remoteHost + '/demo/prefetch',
-        remote: {
-            url: remoteHost + '/demo/search?q=%QUERY',
-            wildcard: '%QUERY'
-        }
-    });
-
-    // ensure default users are read on initialization
-    engine.get('1090217586', '58502284', '10273252', '24477185');
-
-    function engineWithDefaults(q, sync, async) {
-        if (q === '') {
-            sync(engine.get('1090217586', '58502284', '10273252', '24477185'));
-            async([]);
-        } else {
-            engine.search(q, sync, async);
-        }
-    }
-
     /**
      *===============================================================
      *Typeahead and Bloodhound settings for Marcus suggestion engine
@@ -119,7 +84,48 @@ $(document).ready(function () {
             });
 
 
-    /**$('#demo-input').typeahead({
+
+
+
+    //============================
+    // Examples usage of Typeahead
+    //============================
+    /** var engine, remoteHost, template, empty;
+     $.support.cors = true;
+
+     remoteHost = 'https://typeahead-js-twitter-api-proxy.herokuapp.com';
+     template = Handlebars.compile($("#result-template").html());
+     empty = Handlebars.compile($("#empty-template").html());
+
+     engine = new Bloodhound({
+        identify: function (o) {
+            return o.id_str;
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name', 'screen_name'),
+        dupDetector: function (a, b) {
+            return a.id_str === b.id_str;
+        },
+        prefetch: remoteHost + '/demo/prefetch',
+        remote: {
+            url: remoteHost + '/demo/search?q=%QUERY',
+            wildcard: '%QUERY'
+        }
+    });
+
+     // ensure default users are read on initialization
+     engine.get('1090217586', '58502284', '10273252', '24477185');
+
+     function engineWithDefaults(q, sync, async) {
+        if (q === '') {
+            sync(engine.get('1090217586', '58502284', '10273252', '24477185'));
+            async([]);
+        } else {
+            engine.search(q, sync, async);
+        }
+    }
+
+    $('#demo-input').typeahead({
      hint: $('.Typeahead-hint'),
      menu: $('.Typeahead-menu'),
      minLength: 0,
