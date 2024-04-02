50bbde1f1437dbab9b8eb44201970986746f9fed
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2024-04-02 14:31:07 +0200

    reimplement NaturenSearchBuilder using elastic-java-client. Remove scoring to simplify rewrite

--- src/main/java/no/uib/marcus/search/NaturenSearchBuilder.java
+++ src/main/java/no/uib/marcus/search/NaturenSearchBuilder.java
@@ -1,21 +1,15 @@
 package no.uib.marcus.search;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import no.uib.marcus.common.Params;
+import co.elastic.clients.elasticsearch._types.query_dsl.*;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.search.Highlight;
+import co.elastic.clients.elasticsearch.core.search.HighlightField;
 import no.uib.marcus.common.util.QueryUtils;
+
+import java.util.List;
 import java.util.logging.Logger;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.client.Client;
 import no.uib.marcus.common.util.StringUtils;
-import org.elasticsearch.index.query.FilterBuilders;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.builder.SearchSourceBuilderException;
-
-import java.time.LocalDate;
-
-import static org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders.weightFactorFunction;
-
 /***
  * A search builder for naturen
  * @author Hemed Al Ruwehy
@@ -29,54 +23,56 @@ public class NaturenSearchBuilder extends MarcusSearchBuilder {
     }
 
     @Override
-    public SearchRequestBuilder constructSearchRequest() {
-        QueryBuilder query;
-        SearchRequestBuilder searchRequest = super.constructSearchRequest();
+    public SearchRequest.Builder constructSearchRequest() { {
+        Query query;
+        SearchRequest.Builder searchRequest = super.constructSearchRequest();
 
         try {
             //Set query
             if (StringUtils.hasText(getQueryString())) {
-                query = QueryUtils.buildMarcusQueryString(getQueryString());
+                query = QueryUtils.buildMarcusQueryString(getQueryString()).build()._toQuery();
             } else {
-                query = QueryBuilders.functionScoreQuery(QueryBuilders.matchAllQuery())
-                        .add(FilterBuilders.rangeFilter(Params.DateField.AVAILABLE)
-                                .from(LocalDate.now().minusDays(1)), weightFactorFunction(2))
-                        .add(FilterBuilders.existsFilter("hasThumbnail"), weightFactorFunction(2));
+                query = QueryBuilders.functionScore().functions(List.of(new FunctionScore.Builder().filter(QueryBuilders.exists().field("hasThumbnail").build()._toQuery()).build())).build()._toQuery();
+                // @todo?  skip reimplementing scoring except thumbnails
+                //      .query(QueryBuilders.matchAll().build()._toQuery());functionScoreQuery(QueryBuilders.matchAllQuery())
+                //     .add(FilterBuilders.rangeFilter(Params.DateField.AVAILABLE)
+                //              .from(LocalDate.now().minusDays(1)), weightFactorFunction(2))
+                //      .add(FilterBuilders.existsFilter("hasThumbnail"), weightFactorFunction(2));
 
                 // Restrict search only on type issues
                 if (getTypes() == null || getTypes().length == 0) {
                     // Index types are disabled from ES v6.0, we will therefore need to find
                     // another way to do this restriction
-                    searchRequest.setTypes(BoostType.ISSUE);
+                    //@todo index old type as a field
+                    //   searchRequest.setTypes(BoostType.ISSUE);
                 }
             }
             //Set filtered query, whether with or without filter
             if (getFilter() != null) {
-                searchRequest.setQuery(QueryBuilders.filteredQuery(query, getFilter()));
+                searchRequest.query(QueryBuilders.bool().filter(List.of(getFilter().build())).build()._toQuery());
             } else {
-                searchRequest.setQuery(query);
+                searchRequest.query(query);
             }
             //Set highlighting option
-            searchRequest.addHighlightedField("textContent")
-                    // .setHighlighterNoMatchSize(0) // if no match, return these characters, 0 means return all
-                    // .setHighlighterFragmentSize(500) //default 100
-                    // .setHighlighterNumOfFragments(0) //fragments to return. 0 means return everything
-                    .setHighlighterPreTags("<em class='txt-highlight'>")
-                    .setHighlighterPostTags("</em>");
+            searchRequest.highlight(new Highlight.Builder().fields("textContent", new HighlightField.Builder().preTags("<em class='txt-highlight'>")
+                    .postTags("</em>").build()).build());
+
 
             //Show builder for debugging purpose
             //logger.info(searchRequest.toString());
             //System.out.println(searchRequest.toString());
-        } catch (SearchSourceBuilderException e) {
-            logger.severe("Exception occurred when building search request: " + e.getMostSpecificCause());
+        } finally {
+
         }
         return searchRequest;
     }
 
     //Type to be boosted if nothing is specified
-    static class BoostType {
-        final static String ISSUE = "issue";
+ ////   static class BoostType {
+ //       final static String ISSUE = "issue";
+ //   }
+
     }
 
-
 }
+
