f90fdc42aaf9e0312f11228221230ea24e87864e
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-06-26 13:34:49 +0200

    fix autocomplete

--- src/main/java/no/uib/marcus/search/suggestion/CompletionSuggestion.java
+++ src/main/java/no/uib/marcus/search/suggestion/CompletionSuggestion.java
@@ -3,10 +3,15 @@ package no.uib.marcus.search.suggestion;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.*;
+import co.elastic.clients.json.jackson.JacksonJsonpGenerator;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.json.stream.JsonGenerator;
+import java.io.StringWriter;
 import no.uib.marcus.client.ElasticsearchClientFactory;
 import co.elastic.clients.elasticsearch.core.SearchRequest.Builder;
 
@@ -35,18 +40,21 @@ public class CompletionSuggestion {
      **/
     public static Set<String> getSuggestions(String text, int size, @Nullable String... indices) {
         Set<String> suggestValues = new HashSet<>();
-        Builder builder = new Builder();
         try {
             SearchResponse<ObjectNode> suggestResponse = getSuggestionResponse(text, size, indices);
+            List<Suggestion<ObjectNode>> suggestions = suggestResponse.suggest().get("suggest-test");
+
+            JsonFactory jsonFactory = new JsonFactory();
 
             //Add each option(value) to a set to ensure no repetition
-            for ( Suggestion<ObjectNode> option : suggestResponse
-                    .suggest()
-                    .get("completion_suggestion")
-                    ) {
-                // Lowecasing is done via ubb-rdf-river
-                // suggestValues.add(option.getText().string().toLowerCase());
-                suggestValues.add(option.toString());
+            for ( Suggestion<ObjectNode> suggestion : suggestions)
+
+                     {
+                         List<CompletionSuggestOption<ObjectNode>> options = suggestion.completion().options();
+                         for (CompletionSuggestOption<ObjectNode> option : options) {
+                             logger.info("completion: " + option.text());
+                             suggestValues.add(option.text());
+                         }
             }
         } catch (Exception e) {
             logger.severe("Unable to perform suggestion for text: [" + text + "]. Message: " + e.getLocalizedMessage()) ;
@@ -63,10 +71,11 @@ public class CompletionSuggestion {
      **/
     public static SearchResponse<ObjectNode> getSuggestionResponse(String text, int size, @Nullable String... indices) throws IOException {
         Map<String, FieldSuggester> map = new HashMap<>();
-        map.put(SUGGEST_FIELD,FieldSuggester.of(fs -> fs
-                .completion(cs -> cs.skipDuplicates(true)
-                        .size(size)
+        map.put("suggest-test",FieldSuggester.of(fs -> fs
+            .completion(cs -> cs.skipDuplicates(true)
+                .size(size)
                         .field(SUGGEST_FIELD))));
+        logger.info("getSuggestionResponse: " + map);
 
         Suggester suggester = Suggester.of(sf -> sf.suggesters(map).text(text));
        // Suggester.Builder suggesterBuilder = new Suggester.Builder().completion(new CompletionSuggester.Builder().field(SUGGEST_FIELD).size(size).build()._toFieldSuggester());
@@ -79,6 +88,7 @@ public class CompletionSuggestion {
         }
         builder.size(size);
         SearchResponse<ObjectNode> response = client.search(builder.build(), ObjectNode.class);
+        logger.info("getSuggestionResponse: " + response.suggest().get("suggest-test"));
         return response;
     }
 
