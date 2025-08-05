df4b305316667a73fc37d9954a9c8c94d3b90d11
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-08-05 13:11:38 +0200

    review ServiceName

--- src/main/java/no/uib/marcus/common/ServiceName.java
+++ src/main/java/no/uib/marcus/common/ServiceName.java
@@ -15,7 +15,7 @@ public enum ServiceName {
     MARCUS,
 
     /**
-     * Skeivtarkiv service name
+     * Skeivt Arkiv service name
      */
     SKA,
 
@@ -41,22 +41,22 @@ public enum ServiceName {
     private static final Logger logger = Logger.getLogger(ServiceName.class.getName());
 
     /**
-     * Get corresponding enum from it's string representation
-     * @param serviceString a service parameter passed by user
+     * Get the corresponding enum from its string representation
+     * @param serviceString a service parameter passed by the user
      *
      * @return enum which is the result of a string representation
      */
     public static ServiceName toEnum(String serviceString) {
         ServiceName service;
         try {
-            if(!StringUtils.hasText(serviceString)) {//Use default service, if nothing is specified
+            if(!StringUtils.hasText(serviceString)) {//Use the default service if nothing is specified
                 return ServiceName.MARCUS;
             }
             service = ServiceName.valueOf(serviceString.toUpperCase());
         }
         catch (IllegalArgumentException e){
             logger.severe("Service parameter is not recognized. Found [" + serviceString + "]" +
-                    " but expected one of " + Arrays.asList(ServiceName.values()).toString());
+                    " but expected one of " + Arrays.asList(ServiceName.values()));
             /*Fail and do not continue*/
             throw e;
         }
