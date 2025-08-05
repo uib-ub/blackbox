48044a46d4d1a7a0cdb15d3d6382d5c497f47da2
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-08-05 13:09:44 +0200

    review StringUtils.java

--- src/main/java/no/uib/marcus/common/util/StringUtils.java
+++ src/main/java/no/uib/marcus/common/util/StringUtils.java
@@ -1,23 +1,20 @@
 package no.uib.marcus.common.util;
 /**
  * The StringUtils class provides utility methods for working with strings.
- * The methods are copied from https://github.com/elastic/elasticsearch/blob/1.7/src/main/java/org/elasticsearch/common/Strings.java
+ * The methods are copied from <a href="https://github.com/elastic/elasticsearch/blob/1.7/src/main/java/org/elasticsearch/common/Strings.java">elasticsearch implementation</a>
  * which are no longer available in the new elasticsearch-java client. The helper still exists in
- * https://github.com/elastic/elasticsearch/blob/main/server/src/main/java/org/elasticsearch/common/Strings.java but we don't want
+ * <a href="https://github.com/elastic/elasticsearch/blob/main/server/src/main/java/org/elasticsearch/common/Strings.java">elasticsearch main Strings.java</a>, but we don't want
  * to pull in elasticsearch as a dependence but containsWhitespace is no longer present
  */
 public final class StringUtils {
 
     /**
-     * Implement hasText from elasticsearch helper, using same logic, but less code.
-     *
-     * Since we only use Strings in blackbox, only hasText(String str) is implemented
-     * Comment below contains expected values listed by elasticsearch comments found at
+     * Implement hasText from elasticsearch helper, using the same logic, but less code.
+     * Since we only use Strings in Blackbox, only hasText(String str) is implemented
+     * The Comment below contains expected values listed by elasticsearch comments found at
      * url
      * #L142
-     *
-     *
-     * Check whether the given  has actual text.
+     * Check whether the given String has actual text.
      * More specifically, returns <code>true</code> if the string not <code>null</code>,
      * its length is greater than 0, and it contains at least one non-whitespace character.
      * <p><pre>
@@ -28,15 +25,13 @@ public final class StringUtils {
      * StringUtils.hasText(" 12345 ") = true
      * </pre>
      *
-     * @param str the String to check (may be <code>null</code>)
+     * @param str the String to check (maybe <code>null</code>)
      * @return <code>true</code> if the String is not <code>null</code>,
      *         its length is greater than 0, and it does not contain whitespace only
      *
      */
     public static boolean hasText(String str) {
-        if (str == null || str.isEmpty() || str.isBlank())
-            return false;
-        return true;
+      return str != null && !str.isBlank();
     }
 
     //---------------------------------------------------------------------
@@ -53,12 +48,12 @@ public final class StringUtils {
      * StringUtils.hasLength("Hello") = true
      * </pre>
      *
-     * @param str the CharSequence to check (may be <code>null</code>)
+     * @param str the CharSequence to check (maybe <code>null</code>)
      * @return <code>true</code> if the CharSequence is not null and has length
      * @see #hasText(String)
      */
     public static boolean hasLength(CharSequence str) {
-        return (str != null && str.length() > 0);
+        return (str != null && !str.isEmpty());
     }
     public static boolean containsWhitespace(CharSequence str) {
         if (!hasLength(str)) {
