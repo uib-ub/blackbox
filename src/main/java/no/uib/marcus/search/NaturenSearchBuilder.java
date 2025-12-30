098519de97f2594981ce97422a637876360a81df
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-12-30 12:52:51 +0100

    update NaturenSearchBuilder

--- src/main/java/no/uib/marcus/search/NaturenSearchBuilder.java
+++ src/main/java/no/uib/marcus/search/NaturenSearchBuilder.java
@@ -6,20 +6,24 @@ import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.search.Highlight;
 import co.elastic.clients.elasticsearch.core.search.HighlightField;
 import co.elastic.clients.util.NamedValue;
-import java.util.logging.Level;
 import no.uib.marcus.common.util.QueryUtils;
 
 import java.util.List;
-import java.util.logging.Logger;
 import no.uib.marcus.common.util.StringUtils;
 /***
  * A search builder for naturen
  * @author Hemed Al Ruwehy
  */
 public class NaturenSearchBuilder extends MarcusSearchBuilder {
-
-    private final Logger logger = Logger.getLogger(getClass().getName());
-
+    private static final Highlight HIGHLIGHT =
+        new Highlight.Builder()
+        .fields(new NamedValue<>("textContent",
+            new HighlightField.Builder()
+                .postTags("</em>")
+                .preTags("<em class='txt-highlight'>")
+                .build()
+            ))
+        .build();
     NaturenSearchBuilder(ElasticsearchClient client) {
         super(client);
     }
@@ -33,24 +37,31 @@ public class NaturenSearchBuilder extends MarcusSearchBuilder {
             if (StringUtils.hasText(getQueryString())) {
                 query = QueryUtils.buildMarcusQueryString(getQueryString()).build()._toQuery();
             } else {
-                query = QueryBuilders.functionScore().functions(List.of(new FunctionScore.Builder().filter(QueryBuilders.term().value(BoostType.ISSUE).field("type").build()).filter(QueryBuilders.exists().field("hasThumbnail").build()._toQuery()).build())).build()._toQuery();
+                query = QueryBuilders.functionScore().functions(List.of(
+                    new FunctionScore.Builder()
+                        .filter( new BoolQuery.Builder()
+                            .filter(List.of(QueryBuilders.term()
+                                .value(BoostType.ISSUE).field("type")
+                                .build()._toQuery(),QueryBuilders.exists()
+                                .field("hasThumbnail")
+                                .build()
+                                ._toQuery()))
+                            .build())
+                        .build()))
+                    .build()
+                    ._toQuery();
                 }
-
             //Set filtered query, whether with or without filter
             if (getFilter() != null) {
 
               BoolQuery filterQuery = getFilter().build();
-              logger.log(Level.FINE, "sizes: {0}", filterQuery.filter().size());
               searchRequest
                   .query(QueryBuilders.bool().must(query)
                       .filter(List.of(filterQuery._toQuery())).build()._toQuery());            } else {
-                searchRequest.query(query);
-            HighlightField.Builder highlightFieldBuilder = new HighlightField.Builder();
-            highlightFieldBuilder.postTags("</em>").preTags("<em class='txt-highlight'>");
-            Highlight.Builder highlightBuilder = new Highlight.Builder();
-            highlightBuilder.fields(new NamedValue<>("textContent", highlightFieldBuilder.build()));
-            searchRequest.highlight(highlightBuilder.build());
-        }
+              searchRequest.query(query);
+            }
+            searchRequest.highlight(HIGHLIGHT);
+
         return searchRequest;
     }
 }
